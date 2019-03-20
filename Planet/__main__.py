da56bd4140d8420f2f21d9703127e15801f66f6e
Chenyong Miao <cmiao@huskers.unl.edu>
2019-03-20 12:03:33 -0500

    start working on planet project

---
+++ Planet/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+This package used to searching, clipping, ordering, and viewing satellite imagery on a map 
+"""
+
+from schnablelab.apps.base import dmain
+
+
+if __name__ == '__main__':
+    dmain(__file__)
