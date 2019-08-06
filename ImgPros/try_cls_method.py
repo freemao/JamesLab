a96598559f0844f3c0a99688aafe43bda32a7259
Chenyong Miao <cmiao@chenyong-imac.unl.edu>
2019-08-06 11:47:45 -0500

    remove tmp, redudant files

--- ImgPros/try_cls_method.py
+++
@@ -1,9 +0,0 @@
-"""
-class method the way to generate an instance.
-PIL.fromarray()
-pd.DataFrame.fromarray()
-@classmethod
-def fromarray(cls, array):
-    ...
-    return cls(array) # return an object
-"""
