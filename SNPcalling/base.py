3f6969fbc316e139e6452f450aa1dfb3efefd6a0
cmiao <cmiao@huskers.unl.edu>
2020-05-08 22:39:50 -0500

    add slash for alleles in hmp

--- SNPcalling/base.py
+++ SNPcalling/base.py
@@ -100,6 +100,7 @@ class ParseVCF():
                     sys.exit('unknow genotype in vcf file')
                 genos = '\t'.join(genos)
                 rs, chr, pos = j[2], j[0], j[1]
+                alleles = '/'.join(a1a2[0], a1a2[1])
                 new_line = '\t'.join([rs, a1a2, chr, pos, cen_NA, genos])+'\n'
                 yield new_line
 
